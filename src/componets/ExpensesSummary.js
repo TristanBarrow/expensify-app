9ab5ed936b41218f6ab7824e9af2c8661098c55d
Tristan Barrow <tristanbarrow@Tristans-MacBook-Pro.local>
2018-02-13 20:34:38 -0800

    Added Styles

--- src/componets/ExpensesSummary.js
+++ src/componets/ExpensesSummary.js
@@ -3,14 +3,25 @@ import { connect } from 'react-redux';
 import getVisibleExpenses from '../selectors/expenses';
 import getExpensesTotal from '../selectors/expenses-total';
 import numeral from 'numeral';
+import { Link } from 'react-router-dom';
 
-export const ExpenseSummary = (props) => (
-  <div>
-    Viewing {props.expenseCount} expense{props.expenseCount === 1 ? '' : 's'
-  } for a total of {
-      numeral(props.expensesTotal / 100).format('$0,0.00')}.
-  </div>
-);
+export const ExpenseSummary = ({ expenseCount, expensesTotal }) => {
+  const plural = expenseCount === 1 ? '' : 's';
+  const formatedTotal = numeral(expensesTotal / 100).format('$0,0.00');
+  return (
+    <div className='page-header'>
+      <div className='content-container'>
+        <h1 className='page-header__title'>
+          Viewing <span>{expenseCount}</span> expense{plural} for a total of <span>{formatedTotal}</span>.
+        </h1>
+        <div className='page-header__actions'>
+          <Link className='button' to='/create'>Add Expense</Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 
 const mapStateToProps = (state) => ({
     expenseCount: getVisibleExpenses(state.expenses, state.filters).length,
